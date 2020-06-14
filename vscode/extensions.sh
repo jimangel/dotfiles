33fd56eee76d9649f96782cc3a4d2f1e6b12b618
Jim Angel <jameswangel@gmail.com>
2020-06-13 21:24:48 -0500

    last edit

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -9,6 +9,25 @@ fi
 
 code --install-extension golang.go
 
+
+#go get github.com/mdempsky/gocode
+#go get github.com/uudashr/gopkgs/v2/cmd/gopkgs
+#go get github.com/ramya-rao-a/go-outline
+#go get github.com/acroca/go-symbols
+#go get golang.org/x/tools/cmd/guru
+#go get golang.org/x/tools/cmd/gorename
+#go get github.com/cweill/gotests/...
+#go get github.com/fatih/gomodifytags
+#go get github.com/josharian/impl
+#go get github.com/davidrjenni/reftools/cmd/fillstruct
+#go get github.com/haya14busa/goplay/cmd/goplay
+#go get github.com/godoctor/godoctor
+#go get github.com/go-delve/delve/cmd/dlv
+#go get github.com/stamblerre/gocode
+#go get github.com/rogpeppe/godef
+#go get github.com/sqs/goreturns
+#go get golang.org/x/lint/golint
+
 # move to go get...
 #Installing github.com/mdempsky/gocode SUCCEEDED
 #Installing github.com/uudashr/gopkgs/v2/cmd/gopkgs SUCCEEDED
